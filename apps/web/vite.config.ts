68945d10ed606d7001a110510344e3a5cd6117c4
Jose Montilla <jose.montilla@gmail.com>
2026-09-07 17:41:29 -0400

    fix(web): trying to fix what I broke 2

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_PROXY_TARGET ?? "http://api:8081",
+        target: process.env.VITE_API_PROXY_TARGET ?? "http://localhost:8081",
         changeOrigin: true,
       },
       "/metrics": {
-        target: process.env.VITE_API_PROXY_TARGET ?? "http://api:8081",
+        target: process.env.VITE_API_PROXY_TARGET ?? "http://localhost:8081",
         changeOrigin: true,
       },
     },
